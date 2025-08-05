e0d2ecd47bf9ff94868966b86a82f1446c0f2193
unknownmsv <sinamosvi720@gmail.com>
2025-08-05 16:18:01 +0330

    add setup.py, install.py and edit __init__.py

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="o2security",
     version="0.1.0",
-    author="Your Name",  # نام خود را اینجا وارد کنید
-    author_email="your.email@example.com",  # ایمیل خود را اینجا وارد کنید
+    author="sina",  # نام خود را اینجا وارد کنید
+    author_email="sina@unknownmsv.ir",  # ایمیل خود را اینجا وارد کنید
     description="A simple library for securely managing tokens and secrets.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/o2security",  # آدرس گیت‌هاب پروژه
+    url="https://github.com/unknownmsv/o2security",  # آدرس گیت‌هاب پروژه
     packages=find_packages(exclude=["webapp", "webapp.*"]), # فقط پکیج o2security را شامل شود
     classifiers=[
         "Programming Language :: Python :: 3",
